861a174059385aa0435bac115add1e4ff763a2cf
Brian Ethier <davidbrianethier@gmail.com>
2011-10-26 14:25:05 -0700

    Created new blobwars project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlobWars::Application.config.secret_token = '303046888148137a9fc66cd968aa6b33c1d6ae429e9b265bb22515d84a013334ec5348e8554bbd8878237a0b55c0a587411c0ff912c0f83f87e28dca95410cb4'
