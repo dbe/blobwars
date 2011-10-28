bebd99eec37ad1f90c56b4a6362e39acb3e751dc
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2011-10-27 17:37:20 -0700

    Moved play logic to game manager.

--- app/models/game_manager.rb
+++ app/models/game_manager.rb
@@ -3,6 +3,27 @@ class GameManager < ActiveRecord::Base
   def dimensions
     {'x' => width, 'y' => height}
   end
+  
+  def play
+    #make sure to load the classes file, then instantiate an instance of it
+    load "runner/#{self.game_runner_klass}.rb"
+    game_runner_instance = Object.const_get(self.game_runner_klass.classify).new
+    puts "game runner instance is #{game_runner_instance.class}"
+    i = 1
+    clients = self.game_clients.split.map do |c|
+      # Lets reload each AI class for each run.
+      load "ai/#{c}.rb"
+      #create the AI instance with a new id
+      Object.const_get(c.classify).new(i += 1)
+    end
+    
+    
+    history = game_runner_instance.play(clients, self.width, self.height)
+    self.game_histories.create(:move_history => history[:deltas].map do |delta|
+      {'x' => delta.x, 'y' => delta.y, 'team' => delta.team}
+    end, :winner => history[:winner])
+  end
+  
 end
 
 # == Schema Information
