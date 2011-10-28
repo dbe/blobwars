4e2989d9d9b9361ff91dac385569b4afcc442016
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2011-10-27 17:29:21 -0700

    create fixtures.

---
+++ script/generate_fixture.rb
@@ -0,0 +1,12 @@
+#rails env!
+require File.expand_path('../../config/environment.rb',  __FILE__)
+
+GameManager.delete_all
+GameHistory.delete_all
+
+#generate a random game
+GameManager.create(:game_runner_klass => "blobular", :game_clients => "human_player human_player").game_histories.create( 
+:move_history => Array.new(30) do
+  {'x' => rand(20), 'y' => rand(20), 'team' => rand(3)}
+end,
+:winner => "danny")
