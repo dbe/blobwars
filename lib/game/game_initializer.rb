579652ee942fc8090862fad13a64ddb4e283d3dc
Michel Tricot <michel.tricot@gmail.com>
2011-10-28 10:32:04 -0700

    add missing files (turn and detla) and modify initializer accordingly

--- lib/game/game_initializer.rb
+++ lib/game/game_initializer.rb
@@ -8,15 +8,19 @@ class GameInitializer
     width = game_state.board.size
     height = game_state.board.first.size
     return false if player_count > width * height
-    game_state.players.each do |player|
+    game_state.players.each_index do |object_id|
+        team = game_state.players[object_id].team
       invalid_position = true
       begin
         x = rand(width)
         y = rand(height)
         coord = Coordinate.new(x, y)
-        break if game_state.board[x][y] == GameConstants::BLANK
+        if game_state.board[x][y] == GameConstants::BLANK
+          setup_move = Move.new(x, y, team)
+          game_state.apply_move_and_return_turn(setup_move, object_id)
+          break
+        end
       end while true
-      game_state.apply_move(x, y, player.team)
     end
   end
 end
