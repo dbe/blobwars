4711799aa1333da078e7eab7d81f9a39b99a6c42
Brian Ethier <davidbrianethier@gmail.com>
2011-10-31 17:06:59 -0700

    Added ruby dev kit

---
+++ lib/dev_kits/rb/bot.rb
@@ -0,0 +1,9 @@
+require 'lib'
+
+while true do 
+  GameState.get do |game_state|
+    #Do logic, and set x and y variables of your intended move
+    
+    [x,y]
+  end
+end
---
+++ lib/dev_kits/rb/lib.rb
@@ -0,0 +1,40 @@
+class IO
+  #returns GameState object
+  def self.read_game_state
+    GameState.new(tokenize_input_string(STDIN.readline))
+  end
+  
+  def self.send_move(x,y)
+    puts "#{x},#{y}"
+    STDOUT.flush
+  end
+  
+  private
+  def tokenize_input_string(string)
+    array = string.split(',').map! {|ele| ele.to_i}
+    width = array.shift
+    height = array.shift
+    board = Array.new(width){|ele| array.slice!(0,height) }
+    {:width => width, :height => height, :board => board}
+  end
+end
+
+class GameState 
+  attr_reader :width, :height, :board
+  def initialize(hash)
+    @width = hash.width
+    @height = hash.height
+    @board = hash.board
+  end
+  
+  def self.get
+    IO.send_move(*(yield IO.read_game_state))
+  end
+end
+
+
+# def tokenize_input_string(s)
+#   {:width => (width = (array = string.split(',')).shift.to_i), :height => (h = a.shift.to_i), :board => Array.new(g){|i| a.slice!(0,w).map! {|e| e.to_i}}}
+# end
+
+
